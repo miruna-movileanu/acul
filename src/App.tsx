186a8ac0dfe27b4445a1848c20b35d0d792500bf
Miruna Movileanu <miruna.movileanu@okta.com>
2025-04-15 21:36:53 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import React, { useEffect, Suspense } from "react";
-// import { getCurrentScreen } from "@auth0/auth0-acul-js";
+import { getCurrentScreen } from "@auth0/auth0-acul-js";
 
 const LoginIdScreen = React.lazy(() => import("./screens/LoginId"));
-
+const PasskeyScreen = React.lazy(() => import("./screens/Passkeys"));
 
 const App: React.FC = () => {
   const [screen, setScreen] = React.useState("login-id");
   useEffect(() => {
-    const current = "login-id";//getCurrentScreen();
+    // const current = "login-id";//getCurrentScreen();
+    const current = getCurrentScreen();
     setScreen(current!);
   }, []);
 
   const renderScreen = () => {
+    console.log(`current screen: ${screen}`)
     switch (screen) {
       case "login-id":
         return <LoginIdScreen />;
+      case "passkey-enrollment":
+      case "passkey-enrollment-local":
+        return <PasskeyScreen />;
       default:
         return <>No screen rendered</>;
     }
