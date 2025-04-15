80b4c11c102d8fbe1e04d8ebc2a590c599d1ee9d
Miruna Movileanu <miruna.movileanu@okta.com>
2025-04-15 20:11:16 +0300

    added ACUL to the repo

---
+++ src/screens/Passkeys/index.tsx
@@ -0,0 +1,25 @@
+import PasskeyEnrollment from '@auth0/auth0-acul-js/passkey-enrollment';
+
+export const PasskeyScreen = () => {
+  const handleClick = () => {
+      const passkeyEnrollment = new PasskeyEnrollment();
+      passkeyEnrollment.continuePasskeyEnrollment();
+    };
+    
+  return (
+    <div className="w-[100vw] min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-6 bg-white p-8 rounded-lg shadow-md">
+
+        <button 
+          className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+          onClick={() => handleClick()}
+        >
+          Continue Passkey
+        </button>
+
+      </div>
+    </div>
+  );
+};
+
+export default PasskeyScreen;
